de7968b0602cfc5dc67ca43a5313548f801d978c
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-06 08:41:55 -0500

    Add CategoriesPage component for category selection

---
+++ FILE NAME: app/FILE NAME: components/FILE NAME: components/FILE NAME: components/FILE NAME: components/FILE NAME: app/deals/FILE NAME: app/categories/page.tsx
@@ -0,0 +1,8 @@
+export default function CategoriesPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Categories</h1>
+      <p>Select a category to explore curated products.</p>
+    </main>
+  );
+}
