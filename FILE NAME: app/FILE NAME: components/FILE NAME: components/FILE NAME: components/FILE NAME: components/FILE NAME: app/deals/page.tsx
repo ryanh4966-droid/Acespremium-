1f21b473b49167a7541aec9c876605384ca47f2b
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-06 08:41:18 -0500

    Create DealsPage component for displaying deals

---
+++ FILE NAME: app/FILE NAME: components/FILE NAME: components/FILE NAME: components/FILE NAME: components/FILE NAME: app/deals/page.tsx
@@ -0,0 +1,8 @@
+export default function DealsPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Deals</h1>
+      <p>Browse the latest curated deals from Aces Premium.</p>
+    </main>
+  );
+}
